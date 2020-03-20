e8c5e7f65a13a24b0a818a81b9c50b203aae4b6f
tychurch <51180792+tychurch@users.noreply.github.com>
2020-03-20 21:33:58 +1100

    Basic Testing Scene with implemented movement
    Basic Testing Scene Added with the following:
    
    ~Test Tile Palette
    ~Basic Movement Script with Height Restriction as Temp solution to tilemap Ground Detection
    (possible Solution Raycasting)
    ~collision implemented
    ~temp player is pixels preview image

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private float jumpHeight;
+    private Rigidbody2D playerRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        playerRigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+        speed=10.0f;
+        jumpHeight=6.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("d")){
+            
+             
+            GetComponent<ConstantForce2D>().relativeForce = new Vector3(speed, 0, 1);
+           
+        }
+        if(Input.GetKey("a")){
+            
+             
+            GetComponent<ConstantForce2D>().relativeForce = new Vector3(-speed, 0, 1);
+           
+        }
+        else if(Input.GetKeyUp("a")||Input.GetKeyUp("d")){
+            GetComponent<ConstantForce2D>().relativeForce = new Vector3(0, 0, 1);
+
+         }
+if(playerRigidBody.transform.position.y<1){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidBody.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        }
+}
+    
+    }
+}
