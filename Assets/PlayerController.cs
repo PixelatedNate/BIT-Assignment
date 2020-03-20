51e7ae88de11db8a016cf11087de64e32c9ca873
tychurch <51180792+tychurch@users.noreply.github.com>
2020-03-20 21:39:33 +1100

    Implemented Comments into playerController
    as per the previous update:
    
    implemented the following:
    ~basic movement system with temp height restriction as a bandaid fix for ground check
    ~collision with tilemap for those who haven't used a tilemap before shows collision process
    ~testing scene with pixels preview pretzel image as player image
    ~no animation at current time

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //member variables here
     private float speed;
     private float jumpHeight;
     private Rigidbody2D playerRigidBody;
     // Start is called before the first frame update
     void Start()
     {   
+        //initalize member variables
         playerRigidBody = this.gameObject.GetComponent<Rigidbody2D>();
         speed=10.0f;
         jumpHeight=6.0f;
     }
 
     // Update is called once per frame
+    //movement input is handled here
     void Update()
     {
         if(Input.GetKey("d")){
@@ -34,7 +38,9 @@ public class PlayerController : MonoBehaviour
             GetComponent<ConstantForce2D>().relativeForce = new Vector3(0, 0, 1);
 
          }
-if(playerRigidBody.transform.position.y<1){
+         //Temporary ground solution implemented here till better created
+         //raycasting is a potential fix
+    if(playerRigidBody.transform.position.y<1){
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRigidBody.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
