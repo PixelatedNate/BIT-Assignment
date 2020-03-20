f9a636c6858f81b5fa51c5572849f7b29b079f1a
Nathan Jenkins <50900010+PixelatedNate@users.noreply.github.com>
2020-03-21 03:50:26 +0800

    Update to all current systems.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Serialize Field will show the variables in the Unity Inspector and is generally used whilst developing to allow for quick alterations to variable values
+    // before hard-coding it once it is discovered what values feel good.
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpHeight;
+    private Rigidbody2D playerRigidBody;
+   
+    
+    // Component referencing can be done in Awake, which is called upon boot-up of the program.
+    void Awake()
+    {   
+        playerRigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called during the first frame that *this* script is active.
+    // It's good to initialize variables in Start.
+    private void Start()
+    {
+
+    }
+
+    // Fixed Update is used for anything physics related. Call all methods that manipulate or use physics here.
+    // Fixed Update, like "Regular" Update, is called every frame.
+    private void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            Movement();
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    
+    }
+
+    // "Regular" Update is used for anything *not* physics related. Call all methods that have nothing to do with physics here.
+    // Like Fixed Update, "Regular Update" is called every frame.
+    private void Update()
+    {
+        Animate();
+        Flip();
+    }
+
+    // Sets a temporary float to whatever the Horizontal input of the player is, multiplies it by the speed value, and then assigns
+    // the resulting value to our RigidBody's X velocity. We also ensure our Vertical isn't effected by simply stating that the value is
+    // what ever the current value is on the Y axis.
+    void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        playerRigidBody.velocity = new Vector2(horizontal * Time.deltaTime, playerRigidBody.velocity.y);
+    }
+
+
+    // Similar to Movement, it handles the Jumping via applying a value to the Y axis of the Rigidbody.
+    void Jump()
+    {
+        float vertical = jumpHeight * Time.deltaTime;
+        playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, vertical);
+    }
+
+    // Checks if the player moves the character left or right and flips the graphics to the appropriate orientation.
+    void Flip()
+    {
+        if(Input.GetAxis("Horizontal") > 0.1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(Input.GetAxis("Horizontal") < -0.1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void Animate()
+    {
+
+    }
+}
