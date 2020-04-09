f646c2caa935d73d149059df3d526bdb9572d658
tychurch <s3799266@student.rmit.edu.au>
2020-04-09 23:28:50 +1000

    directions update
    - Added Check for Direction When Shooting
    - Fixed instiated projectile from targeting scene object
    
    //BUG
    Collider hits projectile when firing to rightside

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject playerObject;
     [SerializeField]
     private int speed;
 
@@ -12,14 +11,22 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerObject = GameObject.FindWithTag("Player");
         rbody.velocity = transform.right * speed;
-        
+        if (playerObject.transform.position.x > rbody.transform.position.x)
+        {
+            rbody.velocity = transform.right * speed;
+        }
+       else if (playerObject.transform.position.x < rbody.transform.position.x)
+        {
+            rbody.velocity = -transform.right * speed;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnBecameInvisible()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -94,6 +94,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
+
 void OnCollisionEnter2D(Collision2D collision)
 {
     Debug.Log("Entered");
