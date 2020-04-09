14c64b547c78a8bc91026264cfa11787eba9858e
tychurch <s3799266@student.rmit.edu.au>
2020-04-10 00:04:52 +1000

    update bugfix
    updated projectile spawn z axis so now projectile is visible on all sides when shot

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -11,7 +11,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerObject = GameObject.FindWithTag("Player");
+        playerObject = GameObject.Find("Player");
         rbody.velocity = transform.right * speed;
         if (playerObject.transform.position.x > rbody.transform.position.x)
         {
