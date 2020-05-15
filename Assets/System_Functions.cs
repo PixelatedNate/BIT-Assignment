b000cf092b3bc4b845eb5673677ef9ec6fa12814
Nathan Jenkins <50900010+PixelatedNate@users.noreply.github.com>
2020-05-15 15:34:35 +0800

    Revert "Revert "More Art!""
    This reverts commit e6d4b8c9b234523feaf89393e14bef2d7012d4dd.

---
+++ Assets/System_Functions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class System_Functions : MonoBehaviour
+{
+
+    private Boolean doneCheck;
+
+
+
+    public void sleep(float time) {
+
+        StartCoroutine(Sleeping(time));
+
+    }
+
+
+    public void sleepTillDestroy(float time,GameObject gumExplosion)
+    {
+        StartCoroutine(SleepAndDestroy(time,gumExplosion));
+
+    }
+
+
+
+    private IEnumerator Sleeping(float time)
+    {
+
+        //time to sleep for
+        yield return new WaitForSeconds(time);
+        Debug.Log("Timer Finished");
+
+    }
+
+    private IEnumerator SleepAndDestroy(float time, GameObject gumExplosion)
+    {
+
+        //time to sleep for
+        yield return new WaitForSeconds(time);
+        Debug.Log("Timer Finished");
+        Destroy(gumExplosion);
+    }
+
+
+
+}
